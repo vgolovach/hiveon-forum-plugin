d608cb0572deee2182386dcdf257b76ca4dbce46
Valentyn Holovach <valentyn@holova.ch>
2018-07-11 02:03:19 +0300

    fix

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
 # name: discourse-hiveon-homepage
 # about: Show custom Hiveon homepage
 # version: 1.0
-# author: Webmil
-# url: https://gitlab.com/webmil/discourse-hiveon-homepage
+# author: Val
+# url: https://github.com/vgolovach/hiveon-forum-plugin
 
 enabled_site_setting :hiveon_homepage_enabled
 PLUGIN_NAME = "hiveon_forum_plugin".freeze
